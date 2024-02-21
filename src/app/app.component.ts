d96b00f30dbf04ed3a11aff81b3f40555a08484a
Tomás Pettit <G00419414@atu.ie>
2024-02-21 13:16:35 +0000

    Lab3

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+
+@Component({
+  selector: 'root-directive',
+  standalone: true,
+  imports: [RouterOutlet, AComponent, BComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title:string = 'my-first-app';
+  name:string = "Tomás";
+  age:number = 20;
+}
